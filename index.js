7d5fa58fbfc5242c43481488d2d2cccb9bbf9bab
unknown <yenjyao@gmail.com>
2021-07-14 19:23:35 +0100

    Added routing

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var url = require('url')
+var fs = require('fs')
+
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
+  });
+
+function renderHTML(pathname, res) {
+    fs.readFile(pathname, function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(page404);
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }
+       
+    })
+}
+
+http.createServer(function (req, res) {
+    var pathname = req.url.substring(1) + '.html'
+    if(pathname === '.html') pathname = 'index.html'
+    if(pathname != "favicon.ico") {
+        console.log(pathname)
+        renderHTML(pathname, res)
+    }
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
